d760136cac2dc79212c1c04827eab5b6b77e9bc3
Lester <32542078+lhes23@users.noreply.github.com>
2022-10-01 20:48:13 +0800

    change serializers field to "__all__"

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -4,4 +4,5 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ["id", "title", "content"]
+        # fields = ["id", "title", "content"]
+        fields = "__all__"
