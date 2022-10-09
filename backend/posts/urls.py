1eb26fdf22a0e60aa2929ea36569a21a1ce77ca6
Lester <32542078+lhes23@users.noreply.github.com>
2022-10-09 15:20:05 +0800

    use rest framework viewset and DefaultRouter

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from .views import *
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('',PostViewSet)
+
 urlpatterns = [
-    path('<int:id>', getSinglePost, name="posts"),
-    path('', getAllPosts, name="posts"),
+    # path('<int:id>', getSinglePost, name="posts"),
+    # path('', getAllPosts, name="posts"),
+    path("",include(router.urls))
 ]
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -7,32 +7,40 @@ from rest_framework import status
 from .models import Post
 from .serializers import PostSerializer
 
-@api_view(["GET", "POST"])
-def getAllPosts(request):
-    match request.method:
-        case "GET":
-            posts = Post.objects.all()
-            serializer = PostSerializer(posts,many=True)
-            return Response({"posts":serializer.data})
-        case "POST":
-            serializer = PostSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response({"success":serializer.data},status=status.HTTP_201_CREATED)
-            return Response({"errors":serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+from rest_framework import viewsets
+
+# @api_view(["GET", "POST"])
+# def getAllPosts(request):
+#     match request.method:
+#         case "GET":
+#             posts = Post.objects.all()
+#             serializer = PostSerializer(posts,many=True)
+#             return Response({"posts":serializer.data})
+#         case "POST":
+#             serializer = PostSerializer(data=request.data)
+#             if serializer.is_valid():
+#                 serializer.save()
+#                 return Response({"success":serializer.data},status=status.HTTP_201_CREATED)
+#             return Response({"errors":serializer.errors},status=status.HTTP_400_BAD_REQUEST)
         
 
-@api_view(["GET","PUT","DELETE"])
-def getSinglePost(request,id):
-    try:
-        post = Post.objects.get(id=id)
-    except Post.DoesNotExist:
-        return Response({"errors":"No post found for id " + id},status=status.HTTP_400_BAD_REQUEST)
+# @api_view(["GET","PUT","DELETE"])
+# def getSinglePost(request,id):
+#     try:
+#         post = Post.objects.get(id=id)
+#     except Post.DoesNotExist:
+#         return Response({"errors":"No post found for id " + id},status=status.HTTP_400_BAD_REQUEST)
         
-    match request.method:
-        case "GET":
-            serializer = PostSerializer(post)
-            return Response({"post":serializer.data},status=status.HTTP_200_OK)
-        case "DELETE":
-            post.delete()
-            return Response({"status":"Post deleted!"},status=status.HTTP_202_ACCEPTED)
+#     match request.method:
+#         case "GET":
+#             serializer = PostSerializer(post)
+#             return Response({"post":serializer.data},status=status.HTTP_200_OK)
+#         case "DELETE":
+#             post.delete()
+#             return Response({"status":"Post deleted!"},status=status.HTTP_202_ACCEPTED)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
